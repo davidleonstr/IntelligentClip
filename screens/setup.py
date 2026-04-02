e7a6cb0463c06bc3958577553e1df9ccf536cd45
davidleonstr <davidalfonsoleoncarmona@gmail.com>
2026-04-01 19:40:41 -0600

    Combine two notifications

--- screens/setup.py
+++ screens/setup.py
@@ -139,7 +139,6 @@ class SetupScreen(QFlow.Screen):
         button.setDisabled(True)
 
         self.showNotify(self.Config.texts.notifications.validKey, 'success')
-
         self.showNotify(self.Config.texts.notifications.keySetted, 'success')
 
         redirectNotify = self.showNotify(
